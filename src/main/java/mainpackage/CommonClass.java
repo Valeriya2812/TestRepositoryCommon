82d68c4bd2e078f79ee390a16d42b338033acb9c
Valeriia Chernikova <vchernik@softserveinc.com>
2018-11-08 13:57:00 +0200

    add sout "lera"

--- src/main/java/mainpackage/CommonClass.java
+++ src/main/java/mainpackage/CommonClass.java
@@ -4,6 +4,6 @@ public class CommonClass {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-        System.out.println("osip create this text");
+        System.out.println("lera");
     }
 }
