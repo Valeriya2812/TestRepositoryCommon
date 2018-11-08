5f271c6ab736b88109915c33049ef96d254b951b
osip <osipnunik@gmail.com>
2018-11-08 13:59:15 +0200

    osip add this text 'osip create text second time'

--- src/main/java/mainpackage/CommonClass.java
+++ src/main/java/mainpackage/CommonClass.java
@@ -4,6 +4,7 @@ public class CommonClass {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-        System.out.println("lera");
+        System.out.println("osip create this text");
+        System.out.println("osip create this text second time");
     }
 }
