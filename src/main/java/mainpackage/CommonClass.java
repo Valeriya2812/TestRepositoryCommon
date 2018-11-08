1bb90d89e45279648e9ff068b158755714f238a9
osip <osipnunik@gmail.com>
2018-11-08 13:50:59 +0200

    osip add this text 'osip create text'

--- src/main/java/mainpackage/CommonClass.java
+++ src/main/java/mainpackage/CommonClass.java
@@ -2,6 +2,8 @@ package mainpackage;
 
 public class CommonClass {
     public static void main(String[] args) {
+
         System.out.println("Hello");
+        System.out.println("osip create this text");
     }
 }
